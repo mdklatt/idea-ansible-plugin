12325b528bd978132ade381b3441467e935c027e
Michael Klatt <mdklatt@ou.edu>
2020-05-27 10:47:03 -0500

    Refactored AnsibleConfigurationType class

--- src/main/kotlin/software/mdklatt/idea/ansible/run/AnsibleConfigurationType.kt
+++ src/main/kotlin/software/mdklatt/idea/ansible/run/AnsibleConfigurationType.kt
@@ -1,7 +1,5 @@
 package software.mdklatt.idea.ansible.run
 
-
-import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
@@ -21,22 +19,12 @@ class AnsibleConfigurationType : ConfigurationType {
     }
 
     /**
-     * Returns the description of the configuration type. You may return the same text as the display name of the configuration type.
-     *
-     * @return the description of the configuration type.
-     */
-    override fun getConfigurationTypeDescription(): String {
-        return "Run Ansible commands"
-    }
-
-    /**
      * The ID of the configuration type. Should be camel-cased without dashes, underscores, spaces and quotation marks.
      * The ID is used to store run configuration settings in a project or workspace file and
      * must not change between plugin versions.
      */
-    override fun getId(): String {
-        return "AnsibleConfigurationType"
-    }
+    override fun getId(): String = this::class.java.simpleName
+
 
     /**
      * Returns the display name of the configuration type. This is used, for example, to represent the configuration type in the run
@@ -44,9 +32,14 @@ class AnsibleConfigurationType : ConfigurationType {
      *
      * @return the display name of the configuration type.
      */
-    override fun getDisplayName(): String {
-        return "Ansible"
-    }
+    override fun getDisplayName() = "Ansible"
+
+    /**
+     * Returns the description of the configuration type. You may return the same text as the display name of the configuration type.
+     *
+     * @return the description of the configuration type.
+     */
+    override fun getConfigurationTypeDescription() = "Run Ansible commands"
 
     /**
      * Returns the configuration factories used by this configuration type. Normally each configuration type provides just a single factory.
@@ -55,10 +48,8 @@ class AnsibleConfigurationType : ConfigurationType {
      *
      * @return the run configuration factories.
      */
-    override fun getConfigurationFactories(): Array<ConfigurationFactory> {
-        return arrayOf(
+    override fun getConfigurationFactories() = arrayOf(
             GalaxyConfigurationFactory(this),
             PlaybookConfigurationFactory(this)
         )
-    }
 }
