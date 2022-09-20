6fada1f0d9953196fb74f4965d023c97ae6c31c5
Michael Klatt <mdkklatt@alumni.ou.edu>
2022-09-20 01:00:09 -0500

    Upgraded Gradle plugin dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Adapted from <https://github.com/JetBrains/intellij-platform-plugin-template>.
 
-// TODO: import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -9,16 +8,12 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     // Kotlin support
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.7.10"
 
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.0"
+    id("org.jetbrains.intellij") version "1.9.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "1.1.2"
-    // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    // TODO: id("io.gitlab.arturbosch.detekt") version "1.17.1"
-    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    // TODO: id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("org.jetbrains.changelog") version "1.3.1"
 }
 
 group = properties("pluginGroup")
@@ -29,10 +24,9 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    // TODO: detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
     implementation("org.apache.commons:commons-text:1.8")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
-    testImplementation("org.junit.vintage:junit-vintage-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testImplementation("org.junit.vintage:junit-vintage-engine:5.9.0")
 }
 
 // Configure gradle-intellij-plugin plugin.
@@ -51,34 +45,20 @@ intellij {
 // Configure gradle-changelog-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
-    version = properties("pluginVersion")
-    groups = emptyList()
+    //version = properties("pluginVersion")
+    //groups = emptyList()
 }
 
-// TODO: Enable.
-// Configure detekt plugin.
-// Read more: https://detekt.github.io/detekt/kotlindsl.html
-// detekt {
-//     config = files("./detekt-config.yml")
-//     buildUponDefaultConfig = true
-//
-//     reports {
-//         html.enabled = false
-//         xml.enabled = false
-//         txt.enabled = false
-//     }
-//}
 
 tasks {
+    wrapper {
+        gradleVersion = "7.5.1"
+    }
+
     // Set the compatibility versions to 1.8
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
-
-    // TODO: Enable.
-    // withType<Detekt> {
-    //     jvmTarget = "1.8"
-    // }
 
     patchPluginXml {
         version.set(properties("pluginVersion"))
