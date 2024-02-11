2f05522bb729894c15381c314709e2aaf87ebbcb
Michael Klatt <mdklatt@alumni.ou.edu>
2024-02-11 12:17:10 -0600

    Added context menu support for creating Galaxy run configurations

--- src/main/kotlin/actions/RunConfigurationProducer.kt
+++ src/main/kotlin/actions/RunConfigurationProducer.kt
@@ -2,40 +2,33 @@ package dev.mdklatt.idea.ansible.actions
 
 import com.intellij.execution.actions.ConfigurationContext
 import com.intellij.execution.actions.LazyRunConfigurationProducer
+import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.PsiElement
-import dev.mdklatt.idea.ansible.run.AnsibleConfigurationType
-import dev.mdklatt.idea.ansible.run.PlaybookConfigurationFactory
-import dev.mdklatt.idea.ansible.run.PlaybookRunConfiguration
+import dev.mdklatt.idea.ansible.run.*
+import java.nio.file.Path
 import kotlin.io.path.Path
 import org.jetbrains.yaml.YAMLFileType
 import kotlin.io.path.name
 
 
 /**
- * Create a Playbook run configuration from a context menu.
+ * Base class for creating am Ansible run configuration from a context menu.
  *
- * When this is registered as a runConfigurationProducer extension point,
- * opening a context men (i.e. right-clicking) on a YAML file will give the
- * option to crate a run configuration for it.
+ * When a child class is registered as a runConfigurationProducer extension
+ * point, opening a context men (i.e. right-clicking) on a YAML file will give
+ * the option to crate a run configuration for it.
  *
  * @see <a href="https://plugins.jetbrains.com/docs/intellij/run-configurations.html#creating-a-run-configuration-from-context">Creating a Run Configuration from Context</a>
  */
-class PlaybookConfigurationProducer: LazyRunConfigurationProducer<PlaybookRunConfiguration>() {
-    /**
-     * Get a configuration factory for this producer.
-     *
-     * @return configuration factory
-     */
-    override fun getConfigurationFactory() = PlaybookConfigurationFactory(AnsibleConfigurationType())
-
+abstract class AnsibleConfigurationProducer<Config: RunConfiguration>: LazyRunConfigurationProducer<Config>() {
     /**
      * Initialize a new configuration.
      *
      * @return true if set up was successful
      */
     override fun setupConfigurationFromContext(
-        configuration: PlaybookRunConfiguration,
+        configuration: Config,
         context: ConfigurationContext,
         element: Ref<PsiElement>
     ): Boolean {
@@ -45,12 +38,89 @@ class PlaybookConfigurationProducer: LazyRunConfigurationProducer<PlaybookRunCon
             return false
         }
         val yamlPath = Path(yamlFile.virtualFile?.path ?: return false)
+        return setupConfiguration(configuration, yamlPath)
+    }
+
+    /**
+     * Set up a new configuration.
+     *
+     * @param configuration: new configuration
+     * @param yamlPath: path to file used to create the configuration
+     * @return true if setup was successful
+     */
+    abstract fun setupConfiguration(configuration: Config, yamlPath: Path): Boolean
+}
+
+
+/**
+ * Create a Galaxy run configuration from a context menu.
+ */
+class GalaxyConfigurationProducer: AnsibleConfigurationProducer<GalaxyRunConfiguration>() {
+    /**
+     * Get a configuration factory for this producer.
+     *
+     * @return configuration factory
+     */
+    override fun getConfigurationFactory() = GalaxyConfigurationFactory(AnsibleConfigurationType())
+
+    /**
+     * Set up a new configuration.
+     *
+     * @param configuration: new configuration
+     * @param yamlPath: path to file used to create the configuration
+     * @return true if setup was successful
+     */
+    override fun setupConfiguration(configuration: GalaxyRunConfiguration, yamlPath: Path): Boolean {
+        configuration.let {
+            it.name = "Run Galaxy for ${yamlPath.name}"
+            it.requirements = yamlPath.name
+            it.workDir = yamlPath.parent.toString()
+        }
+        return true
+    }
+
+    /**
+     * Determine if a configuration was created by a specific context.
+     *
+     * @param configuration target run configuration
+     * @param context configuration context
+     * @return true if this configuration is from the context
+     */
+    override fun isConfigurationFromContext(configuration: GalaxyRunConfiguration, context: ConfigurationContext): Boolean {
+        // According to the SDK docs, the intent of this method is to allow
+        // reuse of an existing run configuration within a context. Not sure
+        // under which situations this is called, or what the return value
+        // should be. Here, reuse is not necessary.
+        return false
+    }
+}
+
+
+/**
+ * Create a Playbook run configuration from a context menu.
+ */
+class PlaybookConfigurationProducer: AnsibleConfigurationProducer<PlaybookRunConfiguration>() {
+    /**
+     * Get a configuration factory for this producer.
+     *
+     * @return configuration factory
+     */
+    override fun getConfigurationFactory() = PlaybookConfigurationFactory(AnsibleConfigurationType())
+
+    /**
+     * Set up a new configuration.
+     *
+     * @param configuration: new configuration
+     * @param yamlPath: path to file used to create the configuration
+     * @return true if setup was successful
+     */
+    override fun setupConfiguration(configuration: PlaybookRunConfiguration, yamlPath: Path): Boolean {
         configuration.let {
             it.name = "Run playbook ${yamlPath.name}"
             it.playbooks = mutableListOf(yamlPath.name)
             it.workDir = yamlPath.parent.toString()
         }
-         return true
+        return true
     }
 
     /**
--- src/test/kotlin/actions/RunConfigurationProducerTest.kt
+++ src/test/kotlin/actions/RunConfigurationProducerTest.kt
@@ -4,13 +4,38 @@ import com.intellij.execution.PsiLocation
 import com.intellij.execution.actions.ConfigurationContext
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.testFramework.utils.vfs.getPsiFile
+import dev.mdklatt.idea.ansible.run.GalaxyRunConfiguration
 import dev.mdklatt.idea.ansible.run.PlaybookRunConfiguration
+
+
+/**
+ * Base class for configuration producer unit tests.
+ */
+internal abstract class AnsibleConfigurationProducerTest: BasePlatformTestCase() {
+    /**
+     * Set the path for test resource files.
+     *
+     * @return resource path
+     */
+    override fun getTestDataPath() = "src/test/resources"
+
+    /**
+     *
+     * @param yamlFile:
+     * @returns context for test file
+     */
+    fun context(yamlFile: String): ConfigurationContext {
+        val file = myFixture.copyFileToProject(yamlFile)
+        val location = PsiLocation(file.getPsiFile(project))
+        return ConfigurationContext.createEmptyContextForLocation(location)
+    }
+}
 
 
 /**
  * Unit tests for the PlaybookConfigurationAction class.
  */
-internal class PlaybookConfigurationProducerTest: BasePlatformTestCase() {
+internal class PlaybookConfigurationProducerTest: AnsibleConfigurationProducerTest() {
 
     private lateinit var producer: PlaybookConfigurationProducer
 
@@ -23,22 +48,41 @@ internal class PlaybookConfigurationProducerTest: BasePlatformTestCase() {
     }
 
     /**
-     * Set the path for test resource files.
-     *
-     * @return resource path
+     * Test the createConfigurationFromContext() method.
      */
-    override fun getTestDataPath() = "src/test/resources"
+    fun testCreateConfigurationFromContext() {
+        val testFile = "playbook.yml"
+        val configuration = producer.createConfigurationFromContext(context(testFile))
+        (configuration?.configuration as PlaybookRunConfiguration).let {
+            assertSameElements(setOf(testFile), it.playbooks)
+        }
+    }
+}
+
+
+/**
+ * Unit tests for the GalaxyConfigurationAction class.
+ */
+internal class GalaxyConfigurationProducerTest: AnsibleConfigurationProducerTest() {
+
+    private lateinit var producer: GalaxyConfigurationProducer
+
+    /**
+     * Per-test initialization.
+     */
+    override fun setUp() {
+        super.setUp()
+        producer = GalaxyConfigurationProducer()
+    }
 
     /**
      * Test the createConfigurationFromContext() method.
      */
     fun testCreateConfigurationFromContext() {
-        val file = myFixture.copyFileToProject("playbook.yml")
-        val location = PsiLocation(file.getPsiFile(project))
-        val context = ConfigurationContext.createEmptyContextForLocation(location)
-        val configuration = producer.createConfigurationFromContext(context)
-        (configuration?.configuration as PlaybookRunConfiguration).let {
-            assertSameElements(setOf("playbook.yml"), it.playbooks)
+        val testFile = "requirements.yml"
+        val configuration = producer.createConfigurationFromContext(context(testFile))
+        (configuration?.configuration as GalaxyRunConfiguration).let {
+            assertEquals(testFile, it.requirements)
         }
     }
 }
