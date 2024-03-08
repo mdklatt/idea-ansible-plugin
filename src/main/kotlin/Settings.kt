47d5e2941bb879e8b2fe7569cb437dbb16cf5c3a
Michael Klatt <mdklatt@alumni.ou.edu>
2024-03-08 12:07:08 -0600

    Added project-level settings under Tools > Ansible

---
+++ src/main/kotlin/Settings.kt
@@ -0,0 +1,107 @@
+package dev.mdklatt.idea.ansible
+
+import com.intellij.openapi.components.*
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.options.UiDslUnnamedConfigurable
+import com.intellij.openapi.project.Project
+import com.intellij.ui.dsl.builder.Panel
+import com.intellij.ui.dsl.builder.bindItem
+import com.intellij.ui.dsl.builder.bindText
+import dev.mdklatt.idea.common.map.findFirstKey
+
+
+// Adapted from <https://github.com/tomblachut/svelte-intellij>
+
+
+/**
+ * Python execution target types.
+ */
+enum class InstallType { SYSTEM, VIRTUALENV, DOCKER }
+
+
+/**
+ * Get project-level Ansible settings.
+ */
+fun getAnsibleSettings(project: Project): AnsibleSettingsComponent = project.service<AnsibleSettingsComponent>()
+
+
+/**
+ * Serializable Ansible settings.
+ */
+class AnsibleSettingsState : BaseState() {
+    var configFile by string()
+    var installType by enum(InstallType.SYSTEM)
+    var ansibleLocation by string("ansible")
+}
+
+
+/**
+ * Persistent project-level Ansible settings.
+ */
+@Service(Service.Level.PROJECT)
+@State(name = "AnsibleSettingsComponent", storages = [Storage(StoragePathMacros.WORKSPACE_FILE)])
+class AnsibleSettingsComponent: SimplePersistentStateComponent<AnsibleSettingsState>(AnsibleSettingsState()) {
+}
+
+
+/**
+ * Manage project-level Ansible settings.
+ */
+class AnsibleSettingsConfigurable(project: Project): UiDslUnnamedConfigurable.Simple(), Configurable {
+
+    private val settings = getAnsibleSettings(project)
+    private val installTypeOptions = mapOf(
+        InstallType.SYSTEM to "Executable path:",
+        InstallType.VIRTUALENV to "Python virtualenv:",
+        InstallType.DOCKER to "Docker image name:",
+    )
+
+    // Need local versions of settings to bind to UI widgets  for some unknown
+    // reason. Based on examples like the Svelte plugin, it should be possible
+    // to bind directly to settings.state attribute.
+    // TODO: Can settings.state attributes be used as delegates here?
+
+    private var installType: InstallType
+        get() = settings.state.installType
+        set(value) {
+            settings.state.installType = value
+        }
+
+    private var ansibleLocation: String
+        get() = settings.state.ansibleLocation ?: ""
+        set(value) {
+            settings.state.ansibleLocation = value
+        }
+
+    private var configFile: String
+        get() = settings.state.configFile ?: ""
+        set(value) {
+            settings.state.configFile = value
+        }
+
+    /**
+     * Create the UI component for defining settings.
+     */
+    override fun Panel.createContent() {
+        row {
+            comboBox(installTypeOptions.values).bindItem(
+                getter = { installTypeOptions[installType] },
+                setter = { installType = installTypeOptions.findFirstKey(it)!! }
+            )
+            textField().bindText(::ansibleLocation)
+        }
+        row("Config file:") {
+            textFieldWithBrowseButton(
+                "Ansible Config File",
+                fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor(),
+            ).bindText(::configFile)
+        }
+    }
+
+    /**
+     * Get the display name.
+     */
+    // TODO: Where exactly is this used, because the name in plugin.xml is what the IDE shows.
+    override fun getDisplayName() = "Ansible"
+}
---
+++ src/test/kotlin/SettingsTest.kt
@@ -0,0 +1,76 @@
+/**
+ * Unit tests for Galaxy.kt.
+ */
+package dev.mdklatt.idea.ansible
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+
+// The IDEA platform tests use JUnit3, so method names are used to determine
+// behavior instead of annotations. Notably, test classes are *not* constructed
+// before each test, so setUp() methods should be used for initialization.
+// Also, test functions must be named `testXXX` or they will not be found
+// during automatic discovery.
+
+
+/**
+ * Unit tests for the AnsibleSettingsState class.
+ */
+internal class AnsibleSettingsStateTest : BasePlatformTestCase() {
+
+    private lateinit var state: AnsibleSettingsState
+
+    /**
+     * Per-test initialization.
+     */
+    override fun setUp() {
+        super.setUp()
+        state = AnsibleSettingsState()
+    }
+
+    /**
+     * Test the `ansibleLocation` property.
+     */
+    fun testAnsibleLocation() {
+        assertEquals("ansible", state.ansibleLocation)
+        state.ansibleLocation = "abc"
+        assertEquals("abc", state.ansibleLocation)
+    }
+
+    /**
+     * Test the `ansibleLocation` property.
+     */
+    fun testConfigFile() {
+        assertEquals(state.configFile, null)
+    }
+
+    /**
+     * Test the `ansibleLocation` property.
+     */
+    fun testInstallType() {
+        assertEquals(state.installType, InstallType.SYSTEM)
+        state.installType = InstallType.VIRTUALENV
+        assertEquals(InstallType.VIRTUALENV, state.installType)
+    }
+}
+
+
+internal class AnsibleSettingsConfigurableTest : BasePlatformTestCase() {
+
+    private lateinit var configurable: AnsibleSettingsConfigurable
+
+    /**
+     * Per-test initialization.
+     */
+    override fun setUp() {
+        super.setUp()
+        configurable = AnsibleSettingsConfigurable(project)
+    }
+
+    /**
+     * Test the displayName attribute.
+     */
+    fun testDisplayName() {
+        assertEquals("Ansible", configurable.displayName)
+    }
+}
