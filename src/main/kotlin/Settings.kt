9d55b3537549804b8bca80975235abfdfc05880c
Michael Klatt <mdklatt@alumni.ou.edu>
2024-03-15 16:45:07 -0500

    Modified Ansible location input field

--- src/main/kotlin/Settings.kt
+++ src/main/kotlin/Settings.kt
@@ -1,3 +1,6 @@
+/**
+ * Ansible tool settings.
+ */
 package dev.mdklatt.idea.ansible
 
 import com.intellij.openapi.components.*
@@ -5,9 +8,7 @@ import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.UiDslUnnamedConfigurable
 import com.intellij.openapi.project.Project
-import com.intellij.ui.dsl.builder.Panel
-import com.intellij.ui.dsl.builder.bindItem
-import com.intellij.ui.dsl.builder.bindText
+import com.intellij.ui.dsl.builder.*
 import dev.mdklatt.idea.common.map.findFirstKey
 import kotlin.io.path.Path
 
@@ -25,7 +26,6 @@ enum class InstallType { SYSTEM, VIRTUALENV, DOCKER }
  * Get project-level Ansible settings.
  */
 fun getAnsibleSettings(project: Project): AnsibleSettingsComponent = project.service<AnsibleSettingsComponent>()
-
 
 
 /**
@@ -72,9 +72,9 @@ class AnsibleSettingsConfigurable(project: Project): UiDslUnnamedConfigurable.Si
 
     private val settings = getAnsibleSettings(project)
     private val installTypeOptions = mapOf(
-        InstallType.SYSTEM to "Executable path:",
+        InstallType.SYSTEM to "Ansible executable:",
         InstallType.VIRTUALENV to "Python virtualenv:",
-        InstallType.DOCKER to "Docker image name:",
+        InstallType.DOCKER to "Docker image:",
     )
 
     // Need local versions of settings to bind to UI widgets  for some unknown
@@ -105,16 +105,38 @@ class AnsibleSettingsConfigurable(project: Project): UiDslUnnamedConfigurable.Si
      */
     override fun Panel.createContent() {
         row {
-            comboBox(installTypeOptions.values).bindItem(
-                getter = { installTypeOptions[installType] },
-                setter = { installType = installTypeOptions.findFirstKey(it)!! }
-            )
+            comboBox(installTypeOptions.values).let {
+                it.bindItem(
+                    getter = { installTypeOptions[installType] },
+                    setter = { installType = installTypeOptions.findFirstKey(it)!! }
+                )
+                it.onChanged {
+                    // Various attempts to implement this have failed, such as
+                    // reassigning the value of locationField (see below),
+                    // calling createContent() on the parent component, and
+                    // removing locationField before createLocationField() is
+                    // called.
+
+                    // Update the location input field to reflect the new
+                    // install type. Cannot use `installType` property here
+                    // because changes have not been applied yet.
+                    // TODO: Would that it were this simple, but it is not.
+                    //val newInstallType = installTypeOptions.findFirstKey(it.selectedItem)!!
+                    //locationField = createLocationField(newInstallType)
+                }
+            }
+
+            // This should be a dynamic field based on the install type, but
+            // attempts to update this dynamically when installType changes
+            // have not been successful (see above). For now, use the least
+            // common denominator, a plain text field.
+            // TODO: createLocationField(this, installType)
             textField().bindText(::ansibleLocation)
         }
         row("Config file:") {
             textFieldWithBrowseButton(
                 "Ansible Config File",
-                fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor(),
+                fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFileDescriptor(),
             ).bindText(::configFile)
         }
     }
@@ -122,6 +144,28 @@ class AnsibleSettingsConfigurable(project: Project): UiDslUnnamedConfigurable.Si
     /**
      * Get the display name.
      */
-    // TODO: Where exactly is this used, because the name in plugin.xml is what the IDE shows.
+    // TODO: Where exactly is this used, because the IDE uses the name in plugin.xml.
     override fun getDisplayName() = "Ansible"
+
+    /**
+     * Create the input component for the Ansible location field.
+     *
+     * @param row: row containing the field
+     * @param installType: installation type
+     */
+    private fun createLocationField(row: Row, installType: InstallType): Cell<*> {
+        return when (installType) {
+            InstallType.SYSTEM -> row.textFieldWithBrowseButton(
+                "Ansible Executable",
+                fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFileDescriptor(),
+            ).bindText(::ansibleLocation)
+
+            InstallType.VIRTUALENV -> row.textFieldWithBrowseButton(
+                "Python Virtualenv",
+                fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor(),
+            ).bindText(::ansibleLocation)
+
+            InstallType.DOCKER -> row.textField().bindText(::ansibleLocation)
+        }
+    }
 }
